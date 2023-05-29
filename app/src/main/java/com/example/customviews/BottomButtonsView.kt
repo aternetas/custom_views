fdf7b2ab5a79e1316a98024bdc7913fee0234253
kot lena <aternetas@gmail.com>
2023-05-29 16:21:40 +0300

    add custom view by using tag <merge> & attrs.xml

---
+++ app/src/main/java/com/example/customviews/BottomButtonsView.kt
@@ -0,0 +1,83 @@
+package com.example.customviews
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.customviews.databinding.ButtonsBinding
+
+class BottomButtonsView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyleAttr: Int,
+    defStyleRes: Int
+) : ConstraintLayout(context, attrs, defStyleAttr, defStyleRes) {
+    private val binding: ButtonsBinding
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
+            : this(context, attrs, defStyleAttr, 0)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context) : this(context, null)
+
+    init {
+        val inflater = LayoutInflater.from(context)
+        inflater.inflate(R.layout.buttons, this, true)
+        binding = ButtonsBinding.bind(this)
+        initializeAttributes(attrs, defStyleAttr, defStyleRes)
+    }
+
+    private fun initializeAttributes(attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) {
+        if (attrs == null) return
+        val typedArray = context.obtainStyledAttributes(
+            attrs,
+            R.styleable.BottomButtonsView,
+            defStyleAttr,
+            defStyleRes
+        )
+
+        with(binding) {
+            val positiveButtonText =
+                typedArray.getString(R.styleable.BottomButtonsView_bottomPositiveButtonText)
+            positiveButton.text = positiveButtonText ?: "Ok"
+
+            val positiveButtonColor = typedArray.getColor(
+                    R.styleable.BottomButtonsView_bottomPositiveButtonBackgroundColor,
+                    Color.GREEN
+                )
+            positiveButton.backgroundTintList = ColorStateList.valueOf(positiveButtonColor)
+
+/*            val positiveButtonRippleColor = typedArray.getColor(
+                R.styleable.BottomButtonsView_bottomPositiveButtonRippleColor,
+                Color.RED
+            )*/
+
+            val negativeButtonText =
+                typedArray.getString(R.styleable.BottomButtonsView_bottomNegativeButtonText)
+            negativeButton.text = negativeButtonText ?: "No"
+
+            val negativeButtonColor =
+                typedArray.getColor(
+                    R.styleable.BottomButtonsView_bottomNegativeButtonBackgroundColor,
+                    Color.RED
+                )
+            negativeButton.backgroundTintList = ColorStateList.valueOf(negativeButtonColor)
+
+            val isProgressMode =
+                typedArray.getBoolean(R.styleable.BottomButtonsView_bottomProgressMode, false)
+            if (isProgressMode) {
+                positiveButton.visibility = GONE
+                negativeButton.visibility = GONE
+                progress.visibility = VISIBLE
+            } else {
+                positiveButton.visibility = VISIBLE
+                negativeButton.visibility = VISIBLE
+                progress.visibility = GONE
+            }
+        }
+
+        typedArray.recycle()
+    }
+}
--- app/src/main/java/com/example/customviews/MainActivity.kt
+++ app/src/main/java/com/example/customviews/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.customviews
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.example.customviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,22 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
-
-        setupUi()
-
-        with(binding.bottomButtons) {
-            positiveButton.setOnClickListener {
-                negativeButton.visibility = View.GONE
-                positiveButton.visibility = View.GONE
-                progress.visibility = View.VISIBLE
-            }
-        }
-    }
-
-    private fun setupUi() = with(binding.bottomButtons) {
-        progress.visibility = View.GONE
-        negativeButton.setBackgroundColor(
-            ContextCompat.getColor(applicationContext, R.color.red_200)
-        )
     }
 }
