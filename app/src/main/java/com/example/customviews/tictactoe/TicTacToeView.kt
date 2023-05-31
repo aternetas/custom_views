75eea4000fbcd7da99fdd9a730270e196c566246
kot lena <aternetas@gmail.com>
2023-05-31 19:16:08 +0300

    work w/ listeners & grid's width n' height calculation in onMeasure()

--- app/src/main/java/com/example/customviews/tictactoe/TicTacToeView.kt
+++ app/src/main/java/com/example/customviews/tictactoe/TicTacToeView.kt
@@ -3,6 +3,7 @@ package com.example.customviews.tictactoe
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
 import com.example.customviews.R
 import kotlin.properties.Delegates
@@ -14,14 +15,27 @@ class TicTacToeView(
     defStyleRes: Int
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
     var field: TicTacToeField? = null
+        set(value) {
+            field?.listeners?.remove(listener)
+            field = value
+            field?.listeners?.add(listener)
+            requestLayout()
+            invalidate()
+        }
     private var playerOneColor by Delegates.notNull<Int>()
     private var playerTwoColor by Delegates.notNull<Int>()
     private var gridColor by Delegates.notNull<Int>()
+
+    private var listener: OnFieldChangedListener = {
+
+    }
 
     companion object {
         const val PLAYER_X_COLOR = Color.RED
         const val PLAYER_O_COLOR = Color.BLUE
         const val GRID_COLOR = Color.BLACK
+
+        const val DESIRED_CELL_SIZE = 30F
     }
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
@@ -38,6 +52,44 @@ class TicTacToeView(
         } else {
             initializeDefaultTheme()
         }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        field?.listeners?.add(listener)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        field?.listeners?.remove(listener)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val minWidth = suggestedMinimumWidth + paddingStart + paddingEnd
+        val minHeight = suggestedMinimumHeight + paddingTop + paddingBottom
+
+        val desiredCellSizeInPixels = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            DESIRED_CELL_SIZE,
+            resources.displayMetrics
+        ).toInt()
+        val rows = field?.rows ?: 0
+        val columns = field?.columns ?: 0
+
+        val desiredWidth =
+            minWidth.coerceAtLeast(columns * desiredCellSizeInPixels + paddingStart + paddingEnd)
+        val desiredHeight =
+            minHeight.coerceAtLeast(rows * desiredCellSizeInPixels + paddingTop + paddingBottom)
+
+        setMeasuredDimension(
+            resolveSize(desiredWidth, widthMeasureSpec),
+            resolveSize(desiredHeight, heightMeasureSpec)
+        )
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        TODO("Not yet implemented")
     }
 
     private fun initializeAttributes(attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) {
